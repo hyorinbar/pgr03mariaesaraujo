04fce0c491071a36173b6e3799393d3bf638a594
hyorinbar <mariaeduardasodredearaujo@gmail.com>
2026-09-01 23:29:05 -0300

    estrutura inicial do projeto Maven

---
+++ pgr03mariaesaraujo/src/main/java/com/mycompany/pgr03mariaesaraujo/Pgr03mariaesaraujo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pgr03mariaesaraujo;
+
+/**
+ *
+ * @author guest
+ */
+public class Pgr03mariaesaraujo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
