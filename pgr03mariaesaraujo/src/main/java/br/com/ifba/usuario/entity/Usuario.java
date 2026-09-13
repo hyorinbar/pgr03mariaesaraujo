a478dc588ef8bfe10369264304b4e38825afe57c
Diego <diegojsaraujo@gmail.com>
2026-09-13 17:16:22 -0300

    Criando classe Usuario

---
+++ pgr03mariaesaraujo/src/main/java/br/com/ifba/usuario/entity/Usuario.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ifba.usuario.entity;
+
+/**
+ *
+ * @author guest
+ */
+public class Usuario {
+    //variaveis publicas sem metodos. 
+    public String nome;
+    public String cpf;
+    public String genero;
+    public String dataNascimento;
+    public String tel;
+    public String email;
+    public String senha;
+}
