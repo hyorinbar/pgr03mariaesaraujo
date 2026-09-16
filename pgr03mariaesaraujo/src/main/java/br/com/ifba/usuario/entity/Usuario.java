252172baa44289c9dec6e7c2c93e120a0cfea92b
Diego <diegojsaraujo@gmail.com>
2026-09-16 00:32:26 -0300

    Refatorando classe Usuario
    Alterando todos atributos paea privados, criando seus getters e setters e criando constructor

--- pgr03mariaesaraujo/src/main/java/br/com/ifba/usuario/entity/Usuario.java
+++ pgr03mariaesaraujo/src/main/java/br/com/ifba/usuario/entity/Usuario.java
@@ -10,11 +10,80 @@ package br.com.ifba.usuario.entity;
  */
 public class Usuario {
     //variaveis publicas sem metodos. 
-    public String nome;
-    public String cpf;
-    public String genero;
-    public String dataNascimento;
-    public String tel;
-    public String email;
-    public String senha;
+    private String nome;
+    private String cpf;
+    private String genero;
+    private String dataNascimento;
+    private String tel;
+    private String email;
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, String cpf, String genero, String dataNascimento, String tel, String email, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.genero = genero;
+        this.dataNascimento = dataNascimento;
+        this.tel = tel;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
